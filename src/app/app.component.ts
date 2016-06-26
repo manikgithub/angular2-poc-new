ad37293acd08c4669876f4bc7e35cb98520c3854
manikgithub <manikrepaka@gmail.com>
2016-06-26 19:25:22 -0400

    Added header, body, footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { RouteConfig, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate } from '@angular/router-deprecated';
+//import { ROUTER_DIRECTIVES } from '@angular/router';
+import { RouteConfig, ROUTER_PROVIDERS, CanActivate, ROUTER_DIRECTIVES  } from '@angular/router-deprecated';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from  './home/home.component';
 import { UserListComponent } from './users/user.list.component';
 import { UserService } from './services/user.service';
 import { AuthenticationService } from './services/authentication.service';
+import { AppHeaderComponent } from './app.header.component';
+import { AppBodyComponent } from './app.body.component';
 
 @CanActivate((next, prev) => {
       // This must prove to be true for the component @ this route to load
@@ -20,14 +23,14 @@ import { AuthenticationService } from './services/authentication.service';
   selector: 'app-root',
   templateUrl: 'app.component.html',
   //styleUrls: ['app.component.css'],
-  directives: [UserListComponent],
-  providers: [HTTP_PROVIDERS, UserService, AuthenticationService]
+  directives: [UserListComponent, ROUTER_DIRECTIVES, AppHeaderComponent, AppBodyComponent],
+  providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS, UserService, AuthenticationService ]
    
 })
 
-@RouteConfig([
-    { path: '/', redirectTo: ['Login'] },
-    { path: '/home', as: 'Home', component: HomeComponent },
+@RouteConfig ([
+    //{ path: '/', redirectTo: ['Login'] },
+    { path: '/home', name: 'Home', component: HomeComponent },
     { path: '/login', name: 'Login', component: LoginComponent}
 ])
 
